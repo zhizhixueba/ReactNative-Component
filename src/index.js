4fa281c405a1d3df5ba58a4945f5023c9601e4d1
知之学吧 <121071795+zhizhixueba@users.noreply.github.com>
2023-03-16 10:38:51 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Author: Meng
+ * Date: 2021-09-29
+ * Desc: 全局公共组件
+ */
+ 
+export {default as CharBar} from './charbar/CharBar';
+export {default as CompatButton} from './button/CompatButton';
+export {default as MemoInput} from './input/MemoInput';
+
+export {default as ListView} from './list/ListView';
+export {default as Header} from './header/Header';
